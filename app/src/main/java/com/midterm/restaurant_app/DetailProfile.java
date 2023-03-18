0e7e130479b01578a970114deee1caaec662d49e
xuanhung2002 <xuanhung09052002@gmail.com>
2023-03-19 00:47:33 +0700

    Design UI Sign In, Sign Up, Detail profile

---
+++ app/src/main/java/com/midterm/restaurant_app/DetailProfile.java
@@ -0,0 +1,14 @@
+package com.midterm.restaurant_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_profile);
+    }
+}
