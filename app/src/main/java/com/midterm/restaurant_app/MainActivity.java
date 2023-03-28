0cdb2f925d63f6cbdd8ac4e971e99e0c0a890e1c
viennguyen <viennguyen.280202@gmail.com>
2023-03-29 01:13:25 +0700

    Install firebase and implement Sign in

--- app/src/main/java/com/midterm/restaurant_app/MainActivity.java
+++ app/src/main/java/com/midterm/restaurant_app/MainActivity.java
@@ -1,8 +1,16 @@
 package com.midterm.restaurant_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/midterm/restaurant_app/view/SignIn.java
+++ app/src/main/java/com/midterm/restaurant_app/view/SignIn.java
@@ -1,31 +1,55 @@
 package com.midterm.restaurant_app.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.midterm.restaurant_app.MainActivity;
 import com.midterm.restaurant_app.R;
 import com.midterm.restaurant_app.Reset_pass;
 
 public class SignIn extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText edtUser;
+    private EditText edtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+
+        edtUser = findViewById(R.id.edt_user);
+        edtPass = findViewById(R.id.edt_pass);
 
         Button button_Signin = findViewById(R.id.btn_snin);
         button_Signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignIn.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                if(!edtUser.getText().toString().equals("") && !edtPass.getText().toString().equals("")){
+                    login(edtUser.getText().toString(),edtPass.getText().toString());
+                }
+                else if(edtUser.getText().toString().equals("")){
+                    Toast toast = Toast.makeText(SignIn.this,"Enter your user !", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else if(edtPass.getText().toString().equals("")){
+                    Toast toast = Toast.makeText(SignIn.this,"Enter your password !", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
 
@@ -48,5 +72,23 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void login(String email, String pass){
+        mAuth.signInWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(SignIn.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast toast = Toast.makeText(SignIn.this,"User or Password error", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                });
     }
 }
