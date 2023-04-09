72c3a0bcd332e9f8af95d3538ae4563719fcfa35
TranTHiHuongTrinh <trinhtran.dmx2@gmail.com>
2023-04-09 22:07:43 +0700

    test authend firebase

--- app/src/main/java/com/midterm/restaurant_app/SignIn.java
+++ app/src/main/java/com/midterm/restaurant_app/SignIn.java
@@ -1,17 +1,25 @@
 package com.midterm.restaurant_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class SignIn extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class SignIn extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance()
         setContentView(R.layout.activity_sign_in);
 
         Button button_Signin = findViewById(R.id.btn_snin);
@@ -24,4 +32,5 @@ public class SignIn extends AppCompatActivity {
             }
         });
     }
+
 }
